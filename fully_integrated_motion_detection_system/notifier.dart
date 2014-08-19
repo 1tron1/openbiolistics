8d78e7fef84a308ecdac4a47df6fe55c23084bc7
Daniel McCormack <1tron1@users.noreply.github.com>
2014-08-18 22:58:39 -0400

    Update notifier.dart

--- fully_integrated_motion_detection_system/notifier.dart
+++ fully_integrated_motion_detection_system/notifier.dart
@@ -1,11 +1,35 @@
 import 'package:mailer/mailer.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
+
 
 
 main() {
+  
+   final file = new File('userdata.txt');
+  Stream<List<int>> inputStream = file.openRead();
+
+  inputStream
+    // Decode to UTF8.
+    .transform(UTF8.decoder)
+    // Convert stream to individual lines.
+    .transform(new LineSplitter())
+    // Process results.
+    .listen((String line) {
+        print('$line: ${line.length} bytes');
+      },
+      onDone: () { print('File is now closed.'); },
+      onError: (e) { print(e.toString()); });
+}
+  
+  
+  
+  
   print('Congratualtions, HAL 9000 detected Motion');
   var options = new GmailSmtpOptions()
-    ..username = 'email@gmail.com'
-    ..password = 'password'; 
+    ..username = '$line(1)'
+    ..password = '$line(2)'; 
 
   var emailTransport = new SmtpTransport(options);
 
